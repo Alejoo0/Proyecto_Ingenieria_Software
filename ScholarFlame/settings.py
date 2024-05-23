077cbfa3137f3599375e896ff15af05ad22935c2
_damiancortes <damian.cortes@usm.cl>
2024-05-22 20:36:25 -0400

    templates HTML, bootstrap view,forms.urls

--- ScholarFlame/settings.py
+++ ScholarFlame/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -104,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chile/Santiago'
 
 USE_I18N = True
 
