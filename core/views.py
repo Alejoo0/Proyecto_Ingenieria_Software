1fa584826bab002c232b13a3b4a3fdfba1e74cb7
AlexanderEspB <144269398+AlexanderEspB@users.noreply.github.com>
2024-05-25 18:03:35 -0400

    asd

--- core/views.py
+++ core/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import UsuarioDetalles
 from .forms import CustomUserCreationForm
 
 def home(request):
-    return render (request, 'core/home.html')
+    return render(request, 'core/home.html')
 
+@login_required
 def pregunta(request):
-    
-    return render(request,'registration/pregunta.html')
+    user = request.user
+    try:
+        usuario_detalles = UsuarioDetalles.objects.get(user=user)
+        pregunta_display = usuario_detalles.get_pregunta_display()
+        return render(request, 'registration/pregunta.html', {
+            'usuario_detalles': usuario_detalles,
+            'pregunta_display': pregunta_display
+        })
+    except UsuarioDetalles.DoesNotExist:
+        return redirect('home')  # Or show an error message
 
 def registro(request):
     if request.method == 'POST':
