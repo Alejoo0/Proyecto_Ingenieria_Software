d8c632b6a2eec8b492f6cd82a26de45027928538
Vania Reyes <97697337+00ght@users.noreply.github.com>
2024-05-28 01:07:22 -0400

    Usuarios por niveles educacionales

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from .models import UsuarioDetalles, NIVELES_CHOICES
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render (request, 'core/home.html')
@@ -13,3 +15,19 @@ def registro(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/registro.html', {'form': form})
+
+@login_required
+def home(request):
+    try:
+        usuario_detalles = request.user.usuariodetalles
+        nivel_educacional = usuario_detalles.nivel_educacional
+        segundo_valor_nivel = None
+
+        for tupla in NIVELES_CHOICES:
+            if tupla[0] == nivel_educacional:
+                segundo_valor_nivel = tupla[1]
+                break
+    except UsuarioDetalles.DoesNotExist:
+        segundo_valor_nivel = "No especificado"
+    
+    return render(request, 'core/home.html', {'nivel_educacional': segundo_valor_nivel})
